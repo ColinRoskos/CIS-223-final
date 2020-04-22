e07ec9bcc1ed92536142eab5d7ad38588aa736e5
Colin F. Roskos <cfroskos@gmail.com>
2020-04-22 03:43:20 -0500

    added rk image that the print cube was based off. Completed 2nd row solution. Fixed 3rd row solution.

--- Solver.py
+++ Solver.py
@@ -16,12 +16,9 @@ def solve(cube):
     steps = list()  # step [(side, direction), ... ]
     # _merge_lists(steps, _top_cross(cube))
     # _merge_lists(steps, _top_face(cube))
-    # _merge_lists(steps, _second_row(cube))
+    _merge_lists(steps, _second_row(cube))
     _merge_lists(steps, _bottom_cross(cube))
-    cube.print_cube()
     _merge_lists(steps, _bottom_corners(cube))
-    cube.print_cube()
-    print(steps)
     _reduce_steps(steps)
     print(steps)
     print(len(steps))
@@ -37,15 +34,20 @@ def _top_face(cube):
 
 
 def _second_row(cube):
-    raise (NotImplementedError())
+    steps = list()
+
+    while not _all_edges_solved(cube):
+        if _bottom_row_has_second_row_edge(cube, steps):
+            continue
+        _move_edge_from_second_row(cube, steps)
+
+    return steps
 
 
 def _bottom_cross(cube):
     steps = list()
     _merge_lists(steps, _bottom_make_cross(cube))
-    cube.print_cube()
     _merge_lists(steps, _bottom_place_edges(cube))
-    cube.print_cube()
 
     return steps
 
@@ -53,9 +55,7 @@ def _bottom_cross(cube):
 def _bottom_corners(cube):
     steps = list()
     _merge_lists(steps, _bottom_place_corners(cube))
-    cube.print_cube()
     _merge_lists(steps, _bottom_orient_corners(cube))
-    cube.print_cube()
     return steps
 
 
@@ -93,6 +93,7 @@ def _reduce_steps(step_list):
 
             if step_list[index-1][0] == step_list[index][0] == step_list[index+1][0] :
                 if not( step_list[index-1][1] == step_list[index][1] == step_list[index+1][1] ) :
+                    index += 1
                     continue
                 face = step_list[index][0]
                 orientation = (step_list[index][1] + 1) % 2
@@ -104,6 +105,79 @@ def _reduce_steps(step_list):
                 none_removed = False
 
             index += 1
+
+#   Middle row helper steps
+
+
+def _bottom_row_has_second_row_edge(cube, steps):
+    queryable_edges = ((cube.ORANGE, 6), (cube.BLUE, 3), (cube.RED, 1), (cube.GREEN, 4))  # orage, blue, red, green
+
+    has_bottom_edge = False
+    for query_index in range(len(queryable_edges)):
+        edge, adj = cube.get_cell(queryable_edges[query_index][0], queryable_edges[query_index][1], True)
+        if edge == 5 or adj == 5:
+            continue
+
+        advance = query_index
+        while edge != queryable_edges[advance][0]:
+            advance = (advance + 1) % 4
+            steps.append(cube.rotate(5, 1))
+
+        direction = 0
+        offset = 1
+        if adj != queryable_edges[(advance + 1) % 4][0]:
+            direction = 1
+            offset = -1
+        steps.append(cube.rotate(5, direction))
+        steps.append(cube.rotate(queryable_edges[(advance + offset) % 4][0], direction))
+        steps.append(cube.rotate(5, (direction + 1) % 2))
+        steps.append(cube.rotate(queryable_edges[(advance + offset) % 4][0], (direction + 1) % 2))
+        steps.append(cube.rotate(5, (direction + 1) % 2))
+        steps.append(cube.rotate(queryable_edges[advance][0], (direction + 1) % 2))
+        steps.append(cube.rotate(5, direction))
+        steps.append(cube.rotate(queryable_edges[advance][0], direction))
+
+        has_bottom_edge = True
+    return has_bottom_edge
+
+
+def _move_edge_from_second_row(cube, steps):
+    queryable_edges = ((cube.ORANGE, 3), (cube.BLUE, 1), (cube.RED, 4), (cube.GREEN, 6))  # orage, blue, red, green
+
+    for query_index in range(len(queryable_edges)):
+        edge, adj = cube.get_cell(queryable_edges[query_index][0], queryable_edges[query_index][1], True)
+        if edge == 5:
+            continue
+        if adj == 5:
+            continue
+        if edge == queryable_edges[query_index][0] and adj == queryable_edges[(query_index + 1) % 4][0]:
+            continue
+
+        direction = 0
+        offset = 1
+        steps.append(cube.rotate(5, direction))
+        steps.append(cube.rotate(queryable_edges[(query_index + offset) % 4][0], direction))
+        steps.append(cube.rotate(5, (direction + 1) % 2))
+        steps.append(cube.rotate(queryable_edges[(query_index + offset) % 4][0], (direction + 1) % 2))
+        steps.append(cube.rotate(5, (direction + 1) % 2))
+        steps.append(cube.rotate(queryable_edges[query_index][0], (direction + 1) % 2))
+        steps.append(cube.rotate(5, direction))
+        steps.append(cube.rotate(queryable_edges[query_index][0], direction))
+
+
+def _all_edges_solved(cube):
+    queryable_edges = ((cube.ORANGE, 3), (cube.BLUE, 1), (cube.RED, 4), (cube.GREEN, 6))
+
+    for query_index in range(len(queryable_edges)):
+        edge, adj = cube.get_cell(queryable_edges[query_index][0], queryable_edges[query_index][1], True)
+        if edge == 5:
+            return 0
+        if adj == 5:
+            return 0
+        if edge != queryable_edges[query_index][0] or adj != queryable_edges[(query_index + 1) % 4][0]:
+            return 0
+
+    return 1
 
 
 #   Bottom helper steps
@@ -141,46 +215,60 @@ def _bottom_orient_corners_alg(cube):
 
     return steps
 
+
 def _bottom_place_corners(cube):
     # corners (TR), (RB), (BL), (LT)
-    sides = [RubiksCube.TOP, RubiksCube.RIGHT, RubiksCube.BOTTOM, RubiksCube.LEFT]
+    sides = [RubiksCube.TOP, RubiksCube.LEFT, RubiksCube.BOTTOM, RubiksCube.RIGHT]
     corners = [7, 5, 0, 2]
-    orientaion_pattern = [  # (L, U, R)
+    orientaion_pattern = [  # (L, U, R) of current algorithm 'forwards'
         (RubiksCube.LEFT, RubiksCube.BACK, RubiksCube.RIGHT),  # Top 'Forwards'
-        (RubiksCube.TOP, RubiksCube.BACK, RubiksCube.BOTTOM),
+        (RubiksCube.BOTTOM, RubiksCube.BACK, RubiksCube.TOP),
         (RubiksCube.RIGHT, RubiksCube.BACK, RubiksCube.LEFT),
-        (RubiksCube.BOTTOM, RubiksCube.BACK, RubiksCube.TOP)
+        (RubiksCube.TOP, RubiksCube.BACK, RubiksCube.BOTTOM)
+    ]
+
+    correct_colors = [
+        (RubiksCube.BACK, RubiksCube.RIGHT, RubiksCube.TOP),
+        (RubiksCube.BACK, RubiksCube.TOP, RubiksCube.LEFT),
+        (RubiksCube.BACK, RubiksCube.LEFT, RubiksCube.BOTTOM),
+        (RubiksCube.BACK, RubiksCube.BOTTOM, RubiksCube.RIGHT)
     ]
 
     # determine orientation
     # order TR, RB, BL, LT
-
-    corner_index = 0
-    index_not_found = True
-    while corner_index < 4 and index_not_found:
-        color, l_adj, r_adj = cube.get_cell(RubiksCube.BACK, corners[corner_index], True)
-        det = (color, l_adj, r_adj)
-        if sides[corner_index] in det and orientaion_pattern[corner_index][0] in det and orientaion_pattern[corner_index][2] in det:
-            index_not_found = False
-        corner_index += 1
-    if corner_index == 4:
-        corner_index = 0
-
     steps = list()
+    corner_index = 0
+    index_found = False
+    while corner_index < 8 and not index_found:
+        color, l_adj, r_adj = cube.get_cell(RubiksCube.BACK, corners[corner_index % 4], True)
+        det = (color, l_adj, r_adj)
+        if correct_colors[corner_index % 4][0] in det and correct_colors[corner_index % 4][1] in det and correct_colors[corner_index % 4][2] in det:
+            index_found = True
+            continue
+        corner_index += 1
+        if corner_index == 4:
+            default = 0
+            limiter = 0
+            while not _bottom_check_corner_positions(cube, orientaion_pattern) and limiter < 2:
+                _merge_lists(steps, _bottom_corner_placer(cube, orientaion_pattern[default]))
+                limiter += 1
+
+    corner_index = corner_index % 4
+
     limiter = 0
     while not _bottom_check_corner_positions(cube, orientaion_pattern) and limiter < 2:
         _merge_lists(steps, _bottom_corner_placer(cube, orientaion_pattern[corner_index]))
         limiter += 1
-    corner_index = 0
-    corner_placed = False
-    while corner_index < 4 and not corner_placed:
-        color, l_adj, r_adj = cube.get_cell(RubiksCube.BACK, 7, True)
-        det = (color, l_adj, r_adj)
-        if sides[0] in det and orientaion_pattern[0][0] in det and orientaion_pattern[0][2] in det:
-            corner_placed = True
-            continue
-        sides.append(cube.rotate(RubiksCube.BACK, RubiksCube.ROTATE_CW))
-        corner_index += 1
+    # corner_index = 0
+    # corner_placed = False
+    # while corner_index < 4 and not corner_placed:
+    #     color, l_adj, r_adj = cube.get_cell(RubiksCube.BACK, 7, True)
+    #     det = (color, l_adj, r_adj)
+    #     if sides[0] in det and orientaion_pattern[0][0] in det and orientaion_pattern[0][2] in det:
+    #         corner_placed = True
+    #         continue
+    #     sides.append(cube.rotate(RubiksCube.BACK, RubiksCube.ROTATE_CW))
+    #     corner_index += 1
 
     return steps
 
@@ -190,9 +278,9 @@ def _bottom_check_corner_positions(cube, orientation_patterns):
     corners = [7, 5, 0, 2]
     orientaion_patterns = [  # (L, U, R)
         (RubiksCube.LEFT, RubiksCube.BACK, RubiksCube.RIGHT),  # Top 'Forwards'
-        (RubiksCube.TOP, RubiksCube.BACK, RubiksCube.BOTTOM),
+        (RubiksCube.BOTTOM, RubiksCube.BACK, RubiksCube.TOP),
         (RubiksCube.RIGHT, RubiksCube.BACK, RubiksCube.LEFT),
-        (RubiksCube.BOTTOM, RubiksCube.BACK, RubiksCube.TOP)
+        (RubiksCube.TOP, RubiksCube.BACK, RubiksCube.BOTTOM)
     ]
     sides = [RubiksCube.TOP, RubiksCube.RIGHT, RubiksCube.BOTTOM, RubiksCube.LEFT]
     while corner_index < 4:
@@ -233,7 +321,6 @@ def _bottom_place_edges(cube):
         if adj == RubiksCube.TOP:
             rotate = (index + 2) % 4
     _merge_lists(steps, _rotate_cross(cube, rotate))
-    cube.print_cube()
     for rot in range(rotate):
         colors.insert(0, colors[-1])
         colors.pop()
@@ -257,7 +344,6 @@ def _bottom_place_edges(cube):
             colors[2] = colors[3]
             colors[3] = color
 
-            cube.print_cube()
         # is right swap.
         else:
             rotation = 1
@@ -308,30 +394,27 @@ def _bottom_swap_edge_left(cube):
 
 def _bottom_make_cross(cube):
     steps = list()
-    if (_check_bottom(cube, 'L')):
+    if (_check_bottom(cube, '.')): # is dot
         _merge_lists(steps, _bottom_cross_alg(cube, 1))
-        steps.append(cube.rotate(RubiksCube.BACK, RubiksCube.ROTATE_CW))
-        steps.append(cube.rotate(RubiksCube.BACK, RubiksCube.ROTATE_CW))
         _merge_lists(steps, _bottom_cross_alg(cube, 4))
-        steps.append(cube.rotate(RubiksCube.BACK, RubiksCube.ROTATE_CW))
-        steps.append(cube.rotate(RubiksCube.BACK, RubiksCube.ROTATE_CW))
         _merge_lists(steps, _bottom_cross_alg(cube, 1))
         return steps
-    if (_check_bottom(cube, '|')):
+
+    if (_check_bottom(cube, 'L')): # is Line
         ell_ori = _get_ell_orientation(cube)
         _merge_lists(steps, _rotate_cross(cube, ell_ori))
         _merge_lists(steps, _bottom_cross_alg(cube, 4))
-        steps.append(cube.rotate(RubiksCube.BACK, RubiksCube.ROTATE_CW))
-        steps.append(cube.rotate(RubiksCube.BACK, RubiksCube.ROTATE_CW))
         _merge_lists(steps, _bottom_cross_alg(cube, 1))
         _merge_lists(steps, _rotate_cross(cube, ell_ori * -1))
         return steps
-    if not (_check_bottom(cube, '+')):
+
+    if not (_check_bottom(cube, '+')): # is line
         line_ori = _get_line_orientation(cube)
         _merge_lists(steps, _rotate_cross(cube, line_ori))
         _merge_lists(steps, _bottom_cross_alg(cube, 1))
         _merge_lists(steps, _rotate_cross(cube, line_ori * -1))
         return steps
+
     return steps
 
 
@@ -343,13 +426,26 @@ def _check_bottom(cube, check):
     :param check:
     :return:
     """
-    if check not in ("L", "|", "+"): # only four cases exist; point, L, line, cross.
+    if check not in (".", "L", "|", "+"): # only four cases exist; point, L, line, cross.
         raise ValueError
 
     bottom = cube.get_cell(5, 6)
     top = cube.get_cell(5, 1)
     left = cube.get_cell(5, 3)
     right = cube.get_cell(5, 4)
+
+    if check == ".":
+        for t_b in (top, bottom):
+            for l_r in (left, right):
+                if t_b == l_r:
+                    if top == bottom:
+                        return False # is cross
+                    return False # is L
+                if top == bottom or left == right:
+                    return False # is line
+                return True # is .
+
+        check = "|"
 
     if check == "L":
         for t_b in (top, bottom):
@@ -358,7 +454,8 @@ def _check_bottom(cube, check):
                     if top == bottom:
                         return False # is cross
                     return True
-        check = "|"
+                if top == bottom or left == right:
+                    return False # is line
 
     if check == "|":
         if top == bottom:
@@ -408,7 +505,7 @@ def _get_line_orientation(cube):
 def _bottom_cross_alg(cube, front_side):
     if front_side not in (1, 4):
         raise ValueError("Must be side 1 or 4")
-    r_l = [3, front_side, 2]    # [ L, F, R ]
+    r_l = [2, front_side, 3]    # [ L, F, R ]
     if front_side == 4:
         r_l.reverse()
     return [cube.rotate(r_l[1], RubiksCube.ROTATE_CW), cube.rotate(r_l[2], RubiksCube.ROTATE_CW),
@@ -442,7 +539,21 @@ def main():
     cube = RubiksCube(face_0,face_1,face_2,face_3,face_4,face_5)
     cube.print_cube()
     solve(cube)
+    cube.print_cube()
 
+    print("\n\n------------------------------------------------------------------\n\n")
+
+    face_0 = RF(0, [0,0,0,0,0,0,0,0])
+    face_1 = RF(1, [4,4,5,2,5,1,1,1])
+    face_2 = RF(2, [5,5,2,2,2,5,5,2])
+    face_3 = RF(3, [3,3,3,3,3,3,4,3])
+    face_4 = RF(4, [4,4,4,1,3,1,4,5])
+    face_5 = RF(5, [2,2,1,1,1,2,5,4])
+
+    cube = RubiksCube(face_0,face_1,face_2,face_3,face_4,face_5)
+    cube.print_cube()
+    solve(cube)
+    cube.print_cube()
 
 if __name__ == "__main__":
     main()
